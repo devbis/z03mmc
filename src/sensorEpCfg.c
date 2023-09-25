1a07da086b86f4e08cf7b235f98c37f8cd20fbd8
Ivan Belokobylskiy <belokobylskij@gmail.com>
2023-09-25 11:03:08 +0400

    add swBuildId the same as in lumi.sensor_ht

--- src/sensorEpCfg.c
+++ src/sensorEpCfg.c
@@ -37,6 +37,9 @@
 #ifndef ZCL_BASIC_MODEL_ID
 #define ZCL_BASIC_MODEL_ID	   {14,'l','u','m','i','.','s','e','n','s','o','r','_','h','t'}
 #endif
+#ifndef ZCL_BASIC_SW_BUILD_ID
+#define ZCL_BASIC_SW_BUILD_ID     	{9,'3','0','0','0','-','0','0','0','1'}
+#endif
 
 
 /**********************************************************************
@@ -103,6 +106,7 @@ zcl_basicAttr_t g_zcl_basicAttrs =
 	.hwVersion		= 0x00,
 	.manuName		= ZCL_BASIC_MFG_NAME,
 	.modelId		= ZCL_BASIC_MODEL_ID,
+	.swBuildId		= ZCL_BASIC_SW_BUILD_ID,
 	.powerSource	= POWER_SOURCE_BATTERY,
 	.deviceEnable	= TRUE,
 };
@@ -117,6 +121,7 @@ const zclAttrInfo_t basic_attrTbl[] =
 	{ ZCL_ATTRID_BASIC_MODEL_ID,     		ZCL_DATA_TYPE_CHAR_STR, ACCESS_CONTROL_READ,  						(u8*)g_zcl_basicAttrs.modelId},
 	{ ZCL_ATTRID_BASIC_POWER_SOURCE, 		ZCL_DATA_TYPE_ENUM8,    ACCESS_CONTROL_READ,  						(u8*)&g_zcl_basicAttrs.powerSource},
 	{ ZCL_ATTRID_BASIC_DEV_ENABLED,  		ZCL_DATA_TYPE_BOOLEAN,  ACCESS_CONTROL_READ | ACCESS_CONTROL_WRITE, (u8*)&g_zcl_basicAttrs.deviceEnable},
+	{ ZCL_ATTRID_BASIC_SW_BUILD_ID,  		ZCL_DATA_TYPE_CHAR_STR, ACCESS_CONTROL_READ,  						(u8*)&g_zcl_basicAttrs.swBuildId},
 
 	{ ZCL_ATTRID_GLOBAL_CLUSTER_REVISION, 	ZCL_DATA_TYPE_UINT16,  	ACCESS_CONTROL_READ,  						(u8*)&zcl_attr_global_clusterRevision},
 };
