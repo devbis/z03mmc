512c15abd0636a11683f0480b331861dcd2d6705
Ivan Belokobylskiy <belokobylskij@gmail.com>
2024-02-04 22:34:42 +0400

    update converter to support new features

---
+++ converters/lywsd03mmc-legacy-z2m.js
@@ -0,0 +1,34 @@
+// works for all z2m, provides basic functionality
+
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const ota = require('zigbee-herdsman-converters/lib/ota');
+const e = exposes.presets;
+
+const definition = {
+    zigbeeModel: ['LYWSD03MMC'],
+    model: 'LYWSD03MMC',
+    vendor: 'Xiaomi',
+    description: 'Temperature & humidity sensor',
+    fromZigbee: [fz.temperature, fz.humidity, fz.battery, fz.hvac_user_interface],
+    toZigbee: [tz.thermostat_temperature_display_mode],
+    configure: async (device, coordinatorEndpoint, logger) => {
+        const endpoint = device.getEndpoint(1);
+        const bindClusters = ['msTemperatureMeasurement', 'msRelativeHumidity', 'genPowerCfg'];
+        await reporting.bind(endpoint, coordinatorEndpoint, bindClusters);
+        await reporting.temperature(endpoint, {min: 10, max: 300, change: 10});
+        await reporting.humidity(endpoint, {min: 10, max: 300, change: 50});
+        await reporting.batteryVoltage(endpoint);
+        await reporting.batteryPercentageRemaining(endpoint);
+    },
+    exposes: [
+        e.temperature(), e.humidity(), e.battery(),
+        exposes.enum('temperature_display_mode', exposes.access.ALL, ['celsius', 'fahrenheit'])
+        .withDescription('The temperature format displayed on the screen'),
+    ],
+    ota: ota.zigbeeOTA,
+};
+
+module.exports = definition;
--- converters/lywsd03mmc.js
+++ converters/lywsd03mmc.js
@@ -1,32 +1,147 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
+// requires zigbee2mqtt v1.34+
+
+const {
+    batteryPercentage,
+    temperature,
+    humidity,
+    enumLookup,
+    binary,
+    numeric,
+    quirkAddEndpointCluster,
+} = require('zigbee-herdsman-converters/lib/modernExtend');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const ota = require('zigbee-herdsman-converters/lib/ota');
-const e = exposes.presets;
+
+const dataType = {
+    boolean: 0x10,
+    uint8: 0x20,
+    uint16: 0x21,
+    int8: 0x28,
+    int16: 0x29,
+    enum8: 0x30,
+};
 
 const definition = {
     zigbeeModel: ['LYWSD03MMC'],
     model: 'LYWSD03MMC',
     vendor: 'Xiaomi',
-    description: 'Temperature & humidity sensor',
-    fromZigbee: [fz.temperature, fz.humidity, fz.battery, fz.hvac_user_interface],
-    toZigbee: [tz.thermostat_temperature_display_mode],
+    description: 'Temperature & humidity sensor with custom firmware',
+    extend: [
+        quirkAddEndpointCluster({
+            endpointID: 1,
+            outputClusters: [],
+            inputClusters: [
+                'genPowerCfg',
+                'msTemperatureMeasurement',
+                'msRelativeHumidity',
+                'hvacUserInterfaceCfg',
+            ],
+        }),
+        batteryPercentage(),
+        temperature({reporting: {min: 10, max: 300, change: 10}}),
+        humidity({reporting: {min: 10, max: 300, change: 50}}),
+        enumLookup({
+            name: 'temperature_display_mode',
+            lookup: {'celsius': 0, 'fahrenheit': 1},
+            cluster: 'hvacUserInterfaceCfg',
+            attribute: 'tempDisplayMode',
+            description: 'The units of the temperature displayed on the device screen.',
+        }),
+        binary({
+            name: 'show_smiley',
+            valueOn: ['SHOW', 1],
+            valueOff: ['HIDE', 0],
+            cluster: 'hvacUserInterfaceCfg',
+            attribute: {ID: 0x0010, type: dataType.boolean},
+            description: 'Whether to show a smiley on the device screen.',
+        }),
+        binary({
+            name: 'enable_display',
+            valueOn: ['ON', 1],
+            valueOff: ['OFF', 0],
+            cluster: 'hvacUserInterfaceCfg',
+            attribute: {ID: 0x0011, type: dataType.boolean},
+            description: 'Whether to turn display on/off.',
+        }),
+        numeric({
+            name: 'temperature_calibration',
+            unit: 'ºC',
+            cluster: 'msTemperatureMeasurement',
+            attribute: {ID: 0x0010, type: dataType.int16},
+            valueMin: -100.0,
+            valueMax: 100.0,
+            valueStep: 0.01,
+            scale: 100,
+            description: 'The temperature calibration, in 0.01° steps.',
+        }),
+        numeric({
+            name: 'humidity_calibration',
+            unit: '%',
+            cluster: 'msRelativeHumidity',
+            attribute: {ID: 0x0010, type: dataType.int16},
+            valueMin: -100.0,
+            valueMax: 100.0,
+            valueStep: 0.01,
+            scale: 100,
+            description: 'The humidity offset is set in 0.01 % steps.',
+        }),
+        numeric({
+            name: 'comfort_temperature_min',
+            unit: 'ºC',
+            cluster: 'hvacUserInterfaceCfg',
+            attribute: {ID: 0x0102, type: dataType.int16},
+            valueMin: -100.0,
+            valueMax: 100.0,
+            scale: 100,
+            description: 'Comfort parameters/Temperature minimum, in 0.01°C steps.',
+        }),
+        numeric({
+            name: 'comfort_temperature_max',
+            unit: 'ºC',
+            cluster: 'hvacUserInterfaceCfg',
+            attribute: {ID: 0x0103, type: dataType.int16},
+            valueMin: -100.0,
+            valueMax: 100.0,
+            scale: 100,
+            description: 'Comfort parameters/Temperature maximum, in 0.01°C steps.',
+        }),
+        numeric({
+            name: 'comfort_humidity_min',
+            unit: '%',
+            cluster: 'hvacUserInterfaceCfg',
+            attribute: {ID: 0x0104, type: dataType.uint16},
+            valueMin: 0.0,
+            valueMax: 100.0,
+            scale: 100,
+            description: 'Comfort parameters/Humidity minimum, in 0.01% steps.',
+        }),
+        numeric({
+            name: 'comfort_humidity_max',
+            unit: '%',
+            cluster: 'hvacUserInterfaceCfg',
+            attribute: {ID: 0x0105, type: dataType.uint16},
+            valueMin: 0.0,
+            valueMax: 100.0,
+            scale: 100,
+            description: 'Comfort parameters/Humidity maximum, in 0.01% steps.',
+        }),
+    ],
+    ota: ota.zigbeeOTA,
     configure: async (device, coordinatorEndpoint, logger) => {
         const endpoint = device.getEndpoint(1);
         const bindClusters = ['msTemperatureMeasurement', 'msRelativeHumidity', 'genPowerCfg'];
         await reporting.bind(endpoint, coordinatorEndpoint, bindClusters);
         await reporting.temperature(endpoint, {min: 10, max: 300, change: 10});
         await reporting.humidity(endpoint, {min: 10, max: 300, change: 50});
-        await reporting.batteryVoltage(endpoint);
         await reporting.batteryPercentageRemaining(endpoint);
+        try {
+            await endpoint.read('hvacThermostat', [0x0010, 0x0011, 0x0102, 0x0103, 0x0104, 0x0105]);
+            await endpoint.read('msTemperatureMeasurement', [0x0010]);
+            await endpoint.read('msRelativeHumidity', [0x0010]);
+        } catch (e) {
+            /* backward compatibility */
+        }
     },
-    exposes: [
-        e.temperature(), e.humidity(), e.battery(),
-        exposes.enum('temperature_display_mode', exposes.access.ALL, ['celsius', 'fahrenheit'])
-        .withDescription('The temperature format displayed on the screen'),
-    ],
-    ota: ota.zigbeeOTA,
 };
 
 module.exports = definition;
